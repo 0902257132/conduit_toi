8672b5f3ae58c3bef8a4f1eef14b0886d34fa703
ToiNguyen <minhtoi2799@gmail.com>
2020-10-11 09:17:24 +0700

    Updates

---
+++ precache-manifest.5b3bcda58fb2796dae19d7e3ce376104.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4517f78a3beba5b8eb6302d2e5807ce",
+    "url": "/conduit_toi/index.html"
+  },
+  {
+    "revision": "a8e0eb1a3bdd1fb543cd",
+    "url": "/conduit_toi/static/css/main.dce4e2cc.chunk.css"
+  },
+  {
+    "revision": "611c2a04134a9c5a19d8",
+    "url": "/conduit_toi/static/js/2.447d8da9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/conduit_toi/static/js/2.447d8da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e0eb1a3bdd1fb543cd",
+    "url": "/conduit_toi/static/js/main.7a955528.chunk.js"
+  },
+  {
+    "revision": "1e99156272ecf40e9504",
+    "url": "/conduit_toi/static/js/runtime-main.f6c3ed72.js"
+  },
+  {
+    "revision": "5bc723f8545ce7c528cf370c5aa54f67",
+    "url": "/conduit_toi/static/media/loading.5bc723f8.gif"
+  }
+]);
